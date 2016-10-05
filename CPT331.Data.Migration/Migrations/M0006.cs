b21dee228c17d4cbcead3f46a34a4a6930f22bd6
Carl Belle <carl.belle@xero.com>
2016-10-05 16:25:02 +1100

    QLD data bis - rough and ready!

---
+++ CPT331.Data.Migration/Migrations/M0006.cs
@@ -0,0 +1,29 @@
+﻿#region Using References
+
+using System;
+
+using FluentMigrator;
+
+using CPT331.Data.Parsers;
+
+#endregion
+
+namespace CPT331.Data.Migration.Migrations
+{
+	[Migration(0006)]
+	public class M0006 : FluentMigrator.Migration
+	{
+		public override void Down()
+		{
+			//	Nothing to see here
+		}
+
+		public override void Up()
+		{
+			//	Obviously this isn't going to work for you if you don't have the file
+			QldXmlParser qldXmlParser = new QldXmlParser(@"C:\Users\carl.belle\Downloads\Crime Data\division_Reported_Offences_Rates.xml");
+			
+			qldXmlParser.Parse();
+		}
+	}
+}
