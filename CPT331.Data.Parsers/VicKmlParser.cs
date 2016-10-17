5546598128a81089145311838c603a27deffb647
s3364859 <Carlyman@CARLYMAN>
2016-10-17 17:57:38 +1100

    More on the VIC parser

--- CPT331.Data.Parsers/VicKmlParser.cs
+++ CPT331.Data.Parsers/VicKmlParser.cs
@@ -22,9 +22,31 @@ namespace CPT331.Data.Parsers
 
 		internal const string VIC = "VIC";
 
+		private static Dictionary<string, string> CreateMappingDictionary(string fileName)
+		{
+			Dictionary<string, string> mappingDictionary = new Dictionary<string, string>();
+
+			string mappingFileName = fileName.Replace($"{VIC}.kml", $"{VIC}-Mapping.xml");
+			if (File.Exists(mappingFileName) == true)
+			{
+				XmlDocument xmlDocument = new XmlDocument();
+				xmlDocument.Load(mappingFileName);
+
+				XmlNodeList xmlNodeList = xmlDocument.SelectNodes("/Workbook/Worksheet/Table/Row[position() > 1]");
+				foreach (XmlNode xmlNode in xmlNodeList)
+				{
+					mappingDictionary.Add(xmlNode.ChildNodes[0].InnerText.Trim(), xmlNode.ChildNodes[1].InnerText.Trim());
+				}
+			}
+
+			return mappingDictionary;
+		}
+
 		protected override void OnParse(string fileName, List<Coordinate> coordinates)
 		{
 			OutputStreams.WriteLine($"Parsing {VIC} data...");
+
+			Dictionary<string, string> mappingDictionary = CreateMappingDictionary(fileName);
 
 			XmlDocument xmlDocument = new XmlDocument();
 			xmlDocument.Load(fileName);
@@ -33,28 +55,37 @@ namespace CPT331.Data.Parsers
 			foreach (XmlNode xmlNode in xmlNodeList)
 			{
 				string name = xmlNode.SelectSingleNode("name").InnerText;
-				Console.WriteLine(name);
-				//	OutputStreams.WriteLine($"Processing {name}...");
-				//	
-				//	XmlNodeList coordinateXmlNodes = xmlNode.SelectNodes("Polygon/outerBoundaryIs/LinearRing/coordinates | MultiGeometry/Polygon/outerBoundaryIs/LinearRing/coordinates");
-				//	string coordinateValues = "";
-				//	
-				//	foreach (XmlNode coordinateXmlNode in coordinateXmlNodes)
-				//	{
-				//		coordinateValues = $"{coordinateValues} {coordinateXmlNode.InnerText}";
-				//	}
-				//	
-				//	coordinates.Clear();
-				//	
-				//	string[] coordinateLines = coordinateValues.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-				//	foreach (string coordinateLine in coordinateLines)
-				//	{
-				//		string[] coordinateParts = coordinateLine.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-				//	
-				//		coordinates.Add(new Coordinate(Double.Parse(coordinateParts[1]), Double.Parse(coordinateParts[0])));
-				//	}
-				//	
-				//	base.Commit(coordinates, name);
+
+				if (mappingDictionary.ContainsKey(name) == true)
+				{
+					string newName = mappingDictionary[name];
+
+					OutputStreams.WriteLine($"Translating {name} to {newName}...");
+
+					name = newName;
+				}
+
+				OutputStreams.WriteLine($"Processing {name}...");
+				
+				XmlNodeList coordinateXmlNodes = xmlNode.SelectNodes("Polygon/outerBoundaryIs/LinearRing/coordinates | MultiGeometry/Polygon/outerBoundaryIs/LinearRing/coordinates");
+				string coordinateValues = "";
+				
+				foreach (XmlNode coordinateXmlNode in coordinateXmlNodes)
+				{
+					coordinateValues = $"{coordinateValues} {coordinateXmlNode.InnerText}";
+				}
+				
+				coordinates.Clear();
+				
+				string[] coordinateLines = coordinateValues.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				foreach (string coordinateLine in coordinateLines)
+				{
+					string[] coordinateParts = coordinateLine.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+				
+					coordinates.Add(new Coordinate(Double.Parse(coordinateParts[1]), Double.Parse(coordinateParts[0])));
+				}
+				
+				base.Commit(coordinates, name);
 			}
 
 			base.OnParse(fileName, coordinates);
