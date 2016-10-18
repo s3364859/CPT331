64583c856a847685ff40d721544a24612a8c0f54
Peter Weller <contact@peterweller.com.au>
2016-10-18 16:40:19 +1100

    Search functionality from MapViewController moved to LocationSearchView

---
+++ CPT331.iOS/CPT331.iOS/LocationSearchView.swift
@@ -0,0 +1,300 @@
+//
+//  SearchBarViewController.swift
+//  CPT331.iOS
+//
+//  Created by Peter Weller on 18/10/2016.
+//  Copyright © 2016 Peter Weller. All rights reserved.
+//
+
+import UIKit
+import MapboxGeocoder
+import SideMenu
+
+protocol LocationSearchDelegate {
+    func menuButtonTapped(button:UIButton)
+    func locationSelected(location:Location)
+    func getUserLocation() -> CLLocation?
+}
+
+@IBDesignable class LocationSearchView: UIView, UITableViewDataSource, UITableViewDelegate {
+    
+    // -----------------------------
+    // MARK: Constants
+    // -----------------------------
+    internal var NIB_NAME = "LocationSearchView"
+    internal var DEFAULT_RESULT_HEIGHT:CGFloat = 50
+    
+    
+    
+    // -----------------------------
+    // MARK: Runtime Variables
+    // -----------------------------
+    var delegate:LocationSearchDelegate?
+    var searchResults = [GeocodedPlacemark]()
+    var searchQuery:String?
+    internal var _resultHeight:CGFloat!
+    
+    
+    
+    // -----------------------------
+    // MARK: Storyboard References
+    // -----------------------------
+    // Parent Views
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var searchBarView: UIVisualEffectView!
+    @IBOutlet weak var searchResultsView: UIVisualEffectView!
+    
+    // Search Bar
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
+    // Search Results
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Constraints
+    @IBOutlet weak var searchBarHeight: NSLayoutConstraint!
+    @IBOutlet weak var searchBarTopMargin: NSLayoutConstraint!
+    @IBOutlet weak var searchBarLeftMargin: NSLayoutConstraint!
+    @IBOutlet weak var searchBarRightMargin: NSLayoutConstraint!
+    @IBOutlet weak var searchResultsHeight: NSLayoutConstraint!
+    @IBOutlet weak var searchResultsMarginTop: NSLayoutConstraint!
+    @IBOutlet weak var searchResultsMarginBottom: NSLayoutConstraint!
+    
+    
+    
+    // -----------------------------
+    // MARK: Storyboard Config
+    // -----------------------------
+    @IBInspectable var barHeight: CGFloat {
+        get {
+            return self.searchBarHeight.constant
+        }
+        
+        set(height) {
+            self.searchBarHeight.constant = height
+        }
+    }
+    
+    @IBInspectable var resultHeight: CGFloat {
+        get {
+            return self._resultHeight
+        }
+        
+        set(height) {
+            self._resultHeight = height
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.searchBarView.layer.cornerRadius
+        }
+        
+        set(radius) {
+            self.searchBarView.layer.cornerRadius = radius
+            self.searchResultsView.layer.cornerRadius = radius
+        }
+    }
+    
+    @IBInspectable var margin: CGFloat {
+        get {
+            return self.searchBarTopMargin.constant
+        }
+        
+        set(constant) {
+            self.searchBarTopMargin.constant = constant
+            self.searchBarLeftMargin.constant = constant
+            self.searchBarRightMargin.constant = constant
+            self.searchResultsMarginTop.constant = constant
+            self.searchResultsMarginBottom.constant = constant
+        }
+    }
+
+    
+    
+    
+    
+    // -----------------------------
+    // MARK: Initializers
+    // -----------------------------
+    // Called when creating your view programmatically
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.xibSetup()
+    }
+    
+    // Called when creating your view within another interface file.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.xibSetup()
+    }
+    
+    func xibSetup() {
+        // Init view
+        self.view = loadViewFromNib()
+        self.view.frame = bounds
+        self.view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        super.addSubview(self.view)
+        
+        // Setup the sidebar menu
+        let menu = UISideMenuNavigationController()
+        menu.leftSide = true
+        SideMenuManager.menuFadeStatusBar = false
+        SideMenuManager.menuLeftNavigationController = menu
+        
+        // Setup serach results table
+        self.searchResultsView.hidden = true
+        self.tableView.backgroundColor = .clearColor()
+        self.tableView.tableFooterView = UIView()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self._resultHeight = self.DEFAULT_RESULT_HEIGHT
+        
+        // Event Bindings
+        self.textField.addTarget(self, action: #selector(LocationSearchView.searchQueryDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LocationSearchView.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(LocationSearchView.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: self.NIB_NAME, bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    
+    
+    // -----------------------------
+    // MARK: UIView Override
+    // -----------------------------
+    // Returns true if touching an interactive element within xib
+    override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
+        for view: UIView in [self.searchBarView, self.searchResultsView] {
+            if view.pointInside(self.convertPoint(point, toView: view), withEvent: event) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    
+    // -----------------------------
+    // MARK: Search Functionality
+    // -----------------------------
+    func searchQueryDidChange(textField:UITextField) {
+        if let query = textField.text {
+            let userLocation = self.delegate?.getUserLocation()
+            
+            LocationManager.sharedInstance.getSearchPredictions(query, relativeToLocation: userLocation, completion: { searchResults in
+                if searchResults != nil {
+                    
+                    // Sort results by distance from user
+                    if userLocation != nil {
+                        self.searchResults = searchResults!.sort({ $0.distanceFrom(userLocation!) < $1.distanceFrom(userLocation!)})
+                    } else {
+                        self.searchResults = searchResults!
+                    }
+                    
+                    self.searchQuery = query
+                    
+                } else {
+                    self.searchResults.removeAll(keepCapacity: false)
+                    self.searchQuery = nil
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    // Hide table if no results, otherwise show
+                    if !self.textField.isFirstResponder() || self.searchResults.count == 0 {
+                        self.searchResultsView.hidden = true
+                    } else {
+                        self.searchResultsView.hidden = false
+                    }
+                    
+                    // Request table update
+                    self.tableView.reloadData()
+                    
+                    // Update search results table to fit all cells
+                    UIView.animateWithDuration(0.5, animations: {
+                        let height = Int(self._resultHeight) * self.searchResults.count
+                        self.searchResultsHeight.constant = CGFloat(height)
+                        self.setNeedsUpdateConstraints()
+                    })
+                })
+            })
+        }
+    }
+    
+    // When the keyboard is shown, update table view bottom constraint to include keyboard height
+    func keyboardWillShow(notification: NSNotification) {
+        
+        let userInfo:NSDictionary = notification.userInfo!
+        let keyboardFrame: CGRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        let offset = keyboardFrame.size.height
+        
+        UIView.animateWithDuration(1, animations: {
+            self.searchResultsMarginBottom.constant = offset + self.searchResultsMarginTop.constant
+            self.layoutIfNeeded()
+        })
+    }
+    
+    // Remove keyboard height offset when closing keyboard
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animateWithDuration(1, animations: {
+            self.searchResultsMarginBottom.constant = self.searchResultsMarginTop.constant
+            self.layoutIfNeeded()
+        })
+    }
+    
+    
+    
+    // -----------------------------
+    // MARK: Search Results
+    // -----------------------------
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return self._resultHeight
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("LocationListCell") as? LocationListCell
+        if cell == nil {
+            // Load the top-level objects from the custom cell XIB.
+            var topLevelObjects = NSBundle.mainBundle().loadNibNamed("LocationListCell", owner: self, options: nil)
+            // Grab a pointer to the first object (presumably the custom cell, as that's all the XIB should contain).
+            cell = topLevelObjects[0] as? LocationListCell
+        }
+        
+        cell?.backgroundColor = UIColor.clearColor()
+        cell?.placemark = self.searchResults[indexPath.row]
+        cell?.userLocation = delegate?.getUserLocation()
+        cell?.update(withAttributedText: self.searchQuery)
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Deselect row immediately, selection is only temporarily shown to indicate user touch
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let placemark = self.searchResults[indexPath.row]
+        
+        // Clear search bar text
+        self.textField.text = ""
+        self.textField.resignFirstResponder()
+        self.searchQueryDidChange(self.textField)
+        
+        // Pass data back to parent view
+        self.delegate?.locationSelected(placemark.asLocation)
+    }
+}
--- CPT331.iOS/CPT331.iOS/MapViewController.swift
+++ CPT331.iOS/CPT331.iOS/MapViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Mapbox
 import MapboxGeocoder
-import SideMenu
 
 
 struct Pair<T:Hashable,U:Hashable> : Hashable {
@@ -29,10 +28,11 @@ func ==<T:Hashable,U:Hashable>(lhs: Pair<T,U>, rhs: Pair<T,U>) -> Bool {
 }
 
 
-class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDelegate, UIGestureRecognizerDelegate, UITableViewDataSource, UITableViewDelegate {
+class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDelegate, UIGestureRecognizerDelegate, LocationSearchDelegate {
     
-    // Constants
-    let searchResultsRowHeight = 50
+    // -----------------------------
+    // MARK: Constants
+    // -----------------------------
     let searchResultSelectZoom:Double = 12.5
     let annotationImage = UIImage(named: "Event-Annotation.png")
     let panOffsets:(top:CGFloat, bottom:CGFloat) = (
@@ -40,20 +40,11 @@ class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDeleg
         bottom: 400 // Subview
     )
     
-    @IBOutlet weak var mapView: MGLMapView!
-    @IBOutlet weak var searchBarView: UIVisualEffectView!
-    @IBOutlet weak var menuButton: UIButton!
-    @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var searchSubmitButton: UIButton!
-    @IBOutlet weak var searchResultsView: UIVisualEffectView!
-    @IBOutlet weak var searchResultsTable: UITableView!
-    @IBOutlet weak var searchResultsTableHeight: NSLayoutConstraint!
-    @IBOutlet weak var searchResultsTableMarginBottom: NSLayoutConstraint!
     
-    // Runtime vars
+    // -----------------------------
+    // MARK: Runtime Variables
+    // -----------------------------
     var viewModel:MapViewModel = MapViewModel()
-    var searchResults = [GeocodedPlacemark]()
-    var searchQuery:String?
     
     // Stores the most recently tapped location label
     // Used to pass the location to child views
@@ -62,31 +53,30 @@ class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDeleg
     
     // Used to prevent map updatesw while panning
     var mapRegionChanging:Bool=false
+    
+    
+    
+    // -----------------------------
+    // MARK: Storyboard References
+    // -----------------------------
+    @IBOutlet weak var mapView: MGLMapView!
+    @IBOutlet weak var locationSearchView: LocationSearchView!
+    
 
+    
+    
+    // -----------------------------
+    // MARK: Main Logic
+    // -----------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Setup the sidebar menu
-        let menu = UISideMenuNavigationController()
-        menu.leftSide = true
-        SideMenuManager.menuFadeStatusBar = false
-        SideMenuManager.menuLeftNavigationController = menu
-        
-        // Setup search bar
-        searchBarView.layer.cornerRadius = 4
-        searchTextField.addTarget(self, action: #selector(MapViewController.searchQueryDidChange(_:)), forControlEvents: UIControlEvents.EditingChanged)
-        
-        // Setup search results
-        searchResultsView.layer.cornerRadius = 4
-        searchResultsView.hidden = true
-        searchResultsTable.backgroundColor = .clearColor()
-        searchResultsTable.tableFooterView = UIView()
-        searchResultsTable.delegate = self
-        searchResultsTable.dataSource = self
-        
         // Setup map
-        mapView.delegate = self
-        viewModel.delegate = self
+        self.mapView.delegate = self
+        self.viewModel.delegate = self
+        
+        // Setup search
+        self.locationSearchView.delegate = self
         
         // Register double tap recognizer so the single tap knows to ignore them
         let doubleTap = UITapGestureRecognizer(target: self, action: nil)
@@ -99,10 +89,6 @@ class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDeleg
         singleTap.delegate = self
         singleTap.cancelsTouchesInView = false
         self.mapView.addGestureRecognizer(singleTap)
-        
-        // Bind observers to listen to keyboard show/hide events
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MapViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MapViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -120,6 +106,36 @@ class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDeleg
             vc.event = event
         }
     }
+    
+    
+    
+    // -----------------------------
+    // MARK: LocationSearchDelegate
+    // -----------------------------
+    func menuButtonTapped(button: UIButton) {
+        print("Menu button tapped!")
+    }
+    
+    func locationSelected(location:Location) {
+        self.panMapView(
+            toCoordinate: location.coordinate,
+            zoomLevel: self.searchResultSelectZoom,
+            useOffsets: true,
+            animated: true
+        )
+        
+        // Show the selected location in subview
+        self.lastLocationTapped = location
+        self.performSegueWithIdentifier("showLocationView", sender: nil)
+    }
+    
+    func getUserLocation() -> CLLocation? {
+        return self.mapView.userLocation?.location
+    }
+    
+    
+    
+    
     
     
     
@@ -331,127 +347,5 @@ class MapViewController: UIViewController, MGLMapViewDelegate, MapViewModelDeleg
                 print(feature.attributes)
             }
         }
-    }
-    
-    
-    
-    
-    /* --------------------------------- *
-     *       Search Functionality        *
-     * --------------------------------- */
-    func searchQueryDidChange(textField:UITextField) {
-        if let query = self.searchTextField.text {
-            LocationManager.sharedInstance.getSearchPredictions(query, relativeToLocation: self.mapView.userLocation?.location, completion: { (searchResults) in
-                if searchResults != nil {
-                    
-                    // Sort results by distance from user
-                    if let userLocation = self.mapView.userLocation?.location {
-                        self.searchResults = searchResults!.sort({ $0.distanceFrom(userLocation) < $1.distanceFrom(userLocation)})
-                    } else {
-                        self.searchResults = searchResults!
-                    }
-                    
-                    self.searchQuery = query
-                    
-                } else {
-                    self.searchResults.removeAll(keepCapacity: false)
-                    self.searchQuery = nil
-                }
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    // Hide table if no results, otherwise show
-                    if !self.searchTextField.isFirstResponder() || self.searchResults.count == 0 {
-                        self.searchResultsView.hidden = true
-                    } else {
-                        self.searchResultsView.hidden = false
-                    }
-                    
-                    // Request table update
-                    self.searchResultsTable.reloadData()
-                    
-                    // Update search results table to fit all cells
-                    UIView.animateWithDuration(0.5, animations: {
-                        let height = self.searchResultsRowHeight * self.searchResults.count
-                        self.searchResultsTableHeight.constant = CGFloat(height)
-                        self.view.setNeedsUpdateConstraints()
-                    })
-                })
-            })
-        }
-    }
-    
-    // When the keyboard is shown, update table view bottom constraint to include keyboard height
-    func keyboardWillShow(notification: NSNotification) {
-        
-        let userInfo:NSDictionary = notification.userInfo!
-        let keyboardFrame: CGRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-        
-        let offset = keyboardFrame.size.height
-        
-        UIView.animateWithDuration(1, animations: {
-            self.searchResultsTableMarginBottom.constant = offset + 10
-            self.view.layoutIfNeeded()
-        })
-    }
-    
-    // Remove keyboard height offset when closing keyboard
-    func keyboardWillHide(notification: NSNotification) {
-        UIView.animateWithDuration(1, animations: {
-            self.searchResultsTableMarginBottom.constant = 10
-            self.view.layoutIfNeeded()
-        })
-    }
-    
-    
-    
-    
-    
-    
-    /* --------------------------------- *
-     *           Tabular Data            *
-     * --------------------------------- */
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.searchResults.count
-    }
-    
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("locationCell", forIndexPath: indexPath) as! LocationListCell
-        
-        cell.placemark = self.searchResults[indexPath.row]
-        cell.userLocation = self.mapView.userLocation
-        cell.update(withAttributedText: self.searchQuery)
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        // Deselect row immediately, selection is only temporarily shown to indicate user touch
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
-        let placemark = self.searchResults[indexPath.row]
-        
-        // Clear search bar text
-        self.searchTextField.text = ""
-        self.searchTextField.resignFirstResponder()
-        self.searchQueryDidChange(self.searchTextField)
-        
-        self.panMapView(
-            toCoordinate: placemark.location.coordinate,
-            zoomLevel: self.searchResultSelectZoom,
-            useOffsets: true,
-            animated: true
-        )
-        
-        // Show the selected location in subview
-        self.lastLocationTapped = placemark.asLocation
-        self.performSegueWithIdentifier("showLocationView", sender: nil)
     }
 }
